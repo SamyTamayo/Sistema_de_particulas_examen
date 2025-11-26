f6eaefe055e6b986c7b38869084999584650a6dc
Samy Tamayo <samytamayo17@gmail.com>
2025-11-26 12:58:37 -0500

    actualizaciones

--- sketch.js
+++ sketch.js
@@ -6,21 +6,17 @@ function setup() {
 }
 
 function draw() {
-  // 1. DIBUJAR EL FONDO (¡Ahora SÍ funciona!)
-  // Fondo negro semi-transparente para crear el rastro (el efecto que te gusta)
+
   background(0, 0, 0, 30); 
 
-  // 2. DIBUJAR LAS LÍNEAS DE CONEXIÓN
   for (let i = 0; i < sp.length - 1; i++) {
     noFill();
     
-    // Línea BLANCA que se desvanece
     stroke(255, 255, 255, sp[i].tVida * 2); 
     
     line(sp[i].pos.x, sp[i].pos.y, sp[i + 1].pos.x, sp[i + 1].pos.y);
   }
   
-  // 3. ACTUALIZAR Y MOSTRAR PARTÍCULAS
   for (let i = sp.length - 1; i >= 0; i--) {
     let particula = sp[i];
     particula.update();
@@ -31,7 +27,6 @@ function draw() {
     }
   }
 
-  // 4. CREAR NUEVA PARTÍCULA
   let np = new Particula(mouseX, mouseY);
   sp.push(np);
 }
